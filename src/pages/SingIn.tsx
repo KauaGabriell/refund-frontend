99bf14762ce25eebd53e7fe044e33571bf284a3d
KauaGabriell <kauagabriell64@gmail.com>
2026-05-28 16:53:50 -0300

    feat(auth): add sign-in button

--- src/pages/SingIn.tsx
+++ src/pages/SingIn.tsx
@@ -1,10 +1,12 @@
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SingIn() {
   return (
     <form action="" className="w-full flex flex-col gap-4">
       <Input required legend="E-mail" type="email" placeholder="Seu Email" />
       <Input required legend="Senha" type="password" placeholder="Sua Senha" />
+      <Button>Entrar</Button>
     </form>
   );
 }
