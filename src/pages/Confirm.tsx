4b7823ced539b75064b0cd07d294f5613ae5a9a5
KauaGabriell <kauagabriell64@gmail.com>
2026-05-29 11:35:14 -0300

    feat(refund): style confirmation page

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,10 +1,26 @@
 import { Navigate, useLocation } from "react-router";
+import okSvg from "../assets/ok.svg";
 export function Confirm() {
   const location = useLocation();
   if (!location.state?.fromSubmit) return <Navigate to="/" />;
   return (
-    <div>
-      <h1>CONFIRMA A BAGAÇA</h1>
+    <div className="bg-gray-500 lg:w-lg rounded-xl flex flex-col items-center p-10 gap-6">
+      <h1 className="text-2xl font-bold text-center text-green-100">
+        Solicitação Enviada
+      </h1>
+
+      <img src={okSvg} alt="Icone de OK" className="w-28" />
+      <p className="text-sm text-gray-200 text-center">
+        Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o
+        setor financeiro irá encontrar em contato com você
+      </p>
+
+      <a
+        href="/"
+        className="w-ful bg-green-100 rounded-lg p-3 text-center text-white hover:bg-green-200 transition ease-linear"
+      >
+        Nova Solicitação
+      </a>
     </div>
   );
 }
