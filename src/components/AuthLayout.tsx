69364ccfa7416a1f7ab1ec525b8a0dda2420025a
KauaGabriell <kauagabriell64@gmail.com>
2026-05-29 15:43:17 -0300

    feat(ui): add loading screen

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -3,8 +3,8 @@ import logoSvg from "../assets/logo.svg";
 
 export function AuthLayout() {
   return (
-    <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center">
-      <main className="bg-gray-500 p-8 rounded-md flex flex-col items-center md:min-w-115.5">
+    <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100 p-8">
+      <main className="bg-gray-500 p-8 rounded-md flex flex-col items-center w-full md:max-w-115.5">
         <img src={logoSvg} alt="refund logo" className="my-8" />
         <Outlet />
       </main>
